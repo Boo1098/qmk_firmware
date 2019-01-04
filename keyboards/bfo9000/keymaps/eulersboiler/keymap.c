5e042215b8d48e5e90fd97517c45d1e18fc95c85
Boo1098 <nathansd1098@gmail.com>
2019-01-03 23:11:53 -0500

    switched numpad to center

--- keyboards/bfo9000/keymaps/eulersboiler/keymap.c
+++ keyboards/bfo9000/keymaps/eulersboiler/keymap.c
@@ -6,8 +6,12 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 #define ANY		KC_ENT
+#define KC_REC  DYN_REC_START1
+#define KC_PLAY DYN_MACRO_PLAY1
+#define KC_STOP DYN_REC_STOP
+//#define KC_REC XXXXXXX
+//#define KC_PLAY XXXXXXX
 
-#define _DYN 1
 enum keycodes {
     QWERTY = SAFE_RANGE,
     COLEMAK,
@@ -23,12 +27,12 @@ enum keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT( \
-    KC_ESC,  KC_NLCK, KC_CALC, KC_BSPC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  DYN_REC_START1, \
-    KC_PSLS, KC_PAST, KC_PMNS, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, \
-    KC_P7,   KC_P8,   KC_P9,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_MPLY, KC_VOLU, \
-    KC_P1,   KC_P2,   KC_P3,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, ANY,     KC_UP,   KC_VOLD, \
-    KC_P0,   XXXXXXX, KC_PDOT, KC_ENT,  KC_LCTL, KC_LALT, KC_LWIN, KC_SPC,  KC_SPC,        KC_SPC,  KC_SPC,  KC_RALT, KC_RWIN, KC_RCTL, DYN_MACRO_PLAY1, KC_LEFT, KC_DOWN, KC_RGHT \
+    KC_BSPC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TAB,  KC_PPLS, KC_BSPC,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_REC, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PSLS, KC_PAST, KC_PMNS,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
+    KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_MPLY, KC_VOLU, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_STOP, KC_UP,   KC_VOLD, \
+    KC_LCTL, KC_LALT, KC_LWIN, KC_PSCR, KC_ENT,  KC_SPC,  KC_P0,   KC_PDOT, KC_ENT,       KC_SPC,  KC_BSPC, KC_RALT, KC_RWIN, KC_RCTL, KC_PLAY, KC_LEFT, KC_DOWN, KC_RGHT \
 )
 
 };
@@ -38,11 +42,16 @@ void backlight_toggle(void) {
     // INSERT CODE HERE: for example, call function to turn on indicator LED.
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record){
-	uint16_t macro_kc = (keycode == MO(_BASE) ? DYN_REC_STOP : keycode);
-
-    if (!process_record_dynamic_macro(macro_kc, record)) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	/*if(keycode==DYN_REC_START1){
+		keycode=DYN_REC_STOP;
+	}else if(keycode==DYN_REC_START2){
+		keycode=DYN_REC_STOP;
+	}*/
+	
+    if (!process_record_dynamic_macro(keycode, record)) {
         return false;
     }
-	return true;
-};
+	
+	return true;	
+}
