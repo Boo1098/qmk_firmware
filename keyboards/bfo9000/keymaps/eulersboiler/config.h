4c271004da141d188bcc1fdb0bc4b64c3f2fda2c
Boo1098 <nathansd1098@gmail.com>
2019-01-04 00:15:56 -0500

    Fixed errors

--- keyboards/bfo9000/keymaps/eulersboiler/config.h
+++ keyboards/bfo9000/keymaps/eulersboiler/config.h
@@ -36,12 +36,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* RGB Config */ 
 
-#define RGB_DI_PIN 8
-#define RGBLED_NUM 12
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
-#defing RGBLIGHT_LIMIT_VAL 255
+#define RGBLIGHT_LIMIT_VAL 255
 #define RGBLIGHT_SLEEP
 
 #endif
