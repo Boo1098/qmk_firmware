44322b187290870589768eb285e6e38aa870524e
Boo1098 <nathansd1098@gmail.com>
2019-01-04 00:32:02 -0500

    added one handed layerM

--- keyboards/bfo9000/keymaps/eulersboiler/keymap.c
+++ keyboards/bfo9000/keymaps/eulersboiler/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #define _BASE 0
+#define _ONE_HAND 1
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -22,7 +23,7 @@ enum keycodes {
     BACKLIT,
     EXT_PLV,
     DYNAMIC_MACRO_RANGE,
-};
+};	
 #include "dynamic_macro.h"
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -30,11 +31,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BSPC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TAB,  KC_PPLS, KC_BSPC,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_PSCR, \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PSLS, KC_PAST, KC_PMNS,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_REC,  KC_MPLY, \
+    MO(_ONE_HAND),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_REC,  KC_MPLY, \
     KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   KC_STOP, KC_VOLU, \
     KC_LCTL, KC_LALT, KC_LWIN, KC_PLAY, KC_ENT,  KC_SPC,  KC_P0,   KC_PDOT, KC_ENT,       KC_SPC,  KC_BSPC, KC_RALT, KC_RWIN, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD \
 )
 
+[_ONE_HAND] = LAYOUT( \
+	_______,_______,_______,_______,_______,_______,_______,_______,_______,	_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+	_______,_______,_______,_______,_______,_______,_______,_______,_______,	_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+	_______,KC_P,	KC_O,	KC_I,	KC_U,	KC_Y,	_______,_______,_______,	_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+	_______,KC_SCLN,KC_L,	KC_K,	KC_J,	KC_H,	_______,_______,_______,	_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+	_______,KC_SLSH,KC_DOT,	KC_COMM,KC_M,	KC_N,	_______,_______,_______,	_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+	_______,_______,_______,_______,_______,_______,_______,_______,_______,	_______,_______,_______,_______,_______,_______,_______,_______,_______  \
 };
 
 // this is called when dynamic macro buffer is full
