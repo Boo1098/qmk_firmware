9be18d3ed56fbf2dec379e0bc91103da986e2dd5
Boo1098 <nathansd1098@gmail.com>
2019-01-05 08:24:51 -0500

    moved macro keys

--- keyboards/bfo9000/keymaps/eulersboiler/keymap.c
+++ keyboards/bfo9000/keymaps/eulersboiler/keymap.c
@@ -30,12 +30,12 @@ enum keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT( \
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TAB,  KC_PPLS, KC_BSPC,      OSL(_ALT),KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  LGUI(KC_L),KC_DEL,KC_PSCR, \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TAB,  KC_PPLS, KC_BSPC,      OSL(_ALT),KC_F7,  KC_REC,  KC_STOP, KC_PLAY,  KC_F11,  LGUI(KC_L),KC_DEL,KC_PSCR, \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PSLS, KC_PAST, KC_PMNS,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    LEFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_REC,  KC_MPLY, \
-    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   KC_STOP, KC_VOLU, \
-    KC_LCTL, KC_LALT, KC_LWIN, KC_PLAY, KC_ENT,  KC_SPC,  KC_P0,   KC_PDOT, KC_ENT,       KC_SPC,  KC_BSPC, KC_RALT, KC_RWIN, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD \
+    LEFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  XXXXXXX, KC_MPLY, \
+    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   XXXXXXX, KC_VOLU, \
+    KC_LCTL, KC_LALT, KC_LWIN, KC_ENT,  KC_ENT,  KC_SPC,  KC_P0,   KC_PDOT, KC_ENT,       KC_SPC,  KC_BSPC, KC_RALT, KC_RWIN, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD \
 ),
 
 [_ONE_HAND] = LAYOUT( \
